1226f2cd32e183df4d4d95e76374d4fc565fa4a5
gulcenurcagiran <gulcenur2@gmail.com>
2026-09-21 15:57:04 +0300

    feat: add POST endpoint for security logs

--- ThreatLens.Api/Program.cs
+++ ThreatLens.Api/Program.cs
@@ -4,29 +4,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var logs = new List<SecurityLog>
 {
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.MapGet("/hello", () =>
-{
-    return new
-    {
-        message = "ThreatLens API is running",
-        version = "0.1"
-    };
-});
-
-app.MapGet("/api/logs", () =>
-{
-    var logs = new List<SecurityLog>
-    {
         new SecurityLog
         {
             IpAddress = "192.168.1.20",
@@ -44,12 +29,36 @@ app.MapGet("/api/logs", () =>
             UserName = "burak",
             Status = "Success"
         }
+};
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    //Swagger
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/hello", () =>
+{
+    return new
+    {
+        message = "ThreatLens API is running",
+        version = "0.1"
     };
-
-    return logs;
-
 });
 
+app.MapGet("/api/logs", () => logs);
+
+app.MapPost("/api/logs", (SecurityLog newLog) =>
+{
+    logs.Add(newLog);
+    return Results.Created("/api/logs", newLog);
+});
 
 
 
